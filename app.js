5d1eb159eb01882588e95ae6fb4001c5963561de
Artur Gawron <artur@gawron.me>
2020-02-03 22:35:37 +0100

    Add getInput value from DOM

--- app.js
+++ app.js
@@ -7,17 +7,23 @@ var budgetController = (function () {
 
 //UI CONTROLLER
 var UIController = (function () {
-
-
-
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
+        }
+    };
 })();
 
 //GLOBAL APP CONTROLLER
-var controller = (function () {
+var controller = (function (budgetCtrl, UICtrl) {
     var ctrlAddItem = function () {
 
         // Get the field input data
-
+        var input = UICtrl.getInput();
         // Add the item to the budget controller
 
         // Add the item to the UI
