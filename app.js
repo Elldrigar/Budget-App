c240b85b0c2d158e7857a3f3388894701d6b82a5
Artur Gawron <artur@gawron.me>
2020-02-03 21:48:43 +0100

    Add TO DO's to controller && eventListener for Keyboard

--- app.js
+++ app.js
@@ -14,9 +14,26 @@ var UIController = (function () {
 
 //GLOBAL APP CONTROLLER
 var controller = (function () {
+    var ctrlAddItem = function () {
+
+        // Get the field input data
+
+        // Add the item to the budget controller
+
+        // Add the item to the UI
+
+        // Calculate the budget
+
+        // Display the budget on the UI
+
+    };
     document.querySelector('.add__btn').addEventListener('click', function () {
         console.log('click')
+    });
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13) {
+
+        }
     })
-
-
 })();
