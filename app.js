0d497082c68b034689e60fc2acdb67ed52b96435
Artur Gawron <artur@gawron.me>
2020-02-03 21:33:19 +0100

    Add EventListener to controller

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var UIController = (function () {
 
 //GLOBAL APP CONTROLLER
 var controller = (function () {
-
+    document.querySelector('.add__btn').addEventListener('click', function () {
+        console.log('click')
+    })
 
 
 })();
