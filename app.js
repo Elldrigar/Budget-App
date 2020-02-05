09d106b1ad8735c83888d7da53dda084d0839587
Artur Gawron <artur@gawron.me>
2020-02-05 16:10:04 +0100

    Add Deleting function

--- app.js
+++ app.js
@@ -89,7 +89,8 @@ var UIController = (function () {
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
 
     };
 
@@ -159,6 +160,7 @@ var controller = (function (budgetCtrl, UICtrl) {
                 ctrlAddItem();
             }
         });
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
 
     var updateBudget = function () {
@@ -184,6 +186,24 @@ var controller = (function (budgetCtrl, UICtrl) {
             UICtrl.clearFields();
             // Calculate & update budget
             updateBudget();
+        }
+    };
+
+    var ctrlDeleteItem = function (event) {
+        var itemID, splitID, type, ID;
+
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if (itemID) {
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = splitID[1];
+
+            // Delete from data 
+
+            // Delete frim UI
+
+            //Update new budget
         }
     };
 
