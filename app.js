fa31d12705021944c9d38ec45d3cc498fe124511
Artur Gawron <artur@gawron.me>
2020-02-06 14:44:58 +0100

    Updating Percentages UI controller

--- app.js
+++ app.js
@@ -132,7 +132,8 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
 
     };
 
@@ -192,6 +193,23 @@ var UIController = (function () {
             }
         },
 
+        displayPercentages: function (percentages) {
+            var fileds = document.querySelectorAll(DOMStrings.expensesPercLabel);
+            var nodeListForEach = function (list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fileds, function (current, index) {
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
+        },
+
         getDOMStrings: function () {
             return DOMStrings;
         }
@@ -226,7 +244,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         //Read % from budget controller
         var percentages = budgetCtrl.getPercentage();
         //Update UI
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
 
     var ctrlAddItem = function () {
